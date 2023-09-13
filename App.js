f003624c0ddc85ff12b4bfad0c09dbdc5e54d289
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-13 17:37:31 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
 
 
 
 function App() {
 
   const [todo, setTodo] = useState(null)
-  //console.log('App rendered')
+  
+  useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(data => setTodo(data)) 
+  }, []);
 
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(data => setTodo(data))
+  console.log('App rendered')
+  console.log(todo)
 
 
   return (
